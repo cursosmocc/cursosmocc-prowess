c873b103a623c92ade33604a60bee67ed8852c9c
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2023-03-07 17:45:04 -0500

    Update RegistroExitosoPage.jsx

--- front/src/home/pages/RegistroExitosoPage.jsx
+++ front/src/home/pages/RegistroExitosoPage.jsx
@@ -1,10 +1,14 @@
-const RegistroEsitosoPage = () => {
+import './style.css';
+const RegistroExitosoPage = () => {
   return (
-    <div>
-      <h1>Registro exitoso</h1>
-      <p>Gracias por registrarte</p>
+    <div className="registro-exitoso-container">
+      <img className="checkmark-icon" src={"https://res.cloudinary.com/dena7lqj3/image/upload/v1678143989/modulomocc/home/RegistroExitoso2_wungok.png"} alt="Ícono de marca de verificación" />
+      <h1 className="titulo-registro-exitoso">¡Registro exitoso!</h1>
+      <p className="texto-registro-exitoso">Gracias por registrarte en nuestro sitio.</p>
+      <p className="texto-registro-exitoso">Por favor. Confirma tu correo electrónico.</p>
     </div>
   );
 };
 
-export default RegistroEsitosoPage;
+export default RegistroExitosoPage;
+
