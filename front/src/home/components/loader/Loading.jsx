21a95df7605f39b0cf732235c21e139464f18d24
ferca88 <66146523+ferca88@users.noreply.github.com>
2023-03-10 17:51:09 -0500

    correcion

--- front/src/home/components/loader/Loading.jsx
+++ front/src/home/components/loader/Loading.jsx
@@ -4,7 +4,7 @@ import "./loader.css";
 const Loading = () => {
   return (
     <div className="container d-flex justify-content-center align-items-center contenedor-loader">
-      <div class="loader"></div>;
+      <div class="loader"></div>
     </div>
   );
 };
